a304c603f0d1487955006a362a32167280249580
Sivert Espeland Husebo <siverteh@uia.no>
2024-04-19 02:00:13 +0200

    Added functional citation gallery

---
+++ Models/CitationViewModel.cs
@@ -0,0 +1,18 @@
+public class CitationViewModel
+{
+    public string ProjectId { get; set; }
+    public string ReferenceType { get; set; }
+    public string Authors { get; set; }
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string Journal { get; set; }
+    public string BookTitle { get; set; }
+    public string Publisher { get; set; }
+    public string Edition { get; set; }
+    public string Volume { get; set; }
+    public string Number { get; set; }
+    public string Pages { get; set; }
+    public string DOI { get; set; }
+    public string URL { get; set; }
+    public string Misc { get; set; }
+}
